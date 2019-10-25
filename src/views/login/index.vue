1ce15ff78715deb6c035e1d6aa3424add8678f1e
wszll123 <1551312275@qq.com>
2019-10-25 18:41:46 +0800

    登录静态页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <el-card>
       <img src="../../assets/logo_index.png" width="200px" alt />
-      <el-form :model=" loginForm" ref="ruleForm">
+      <el-form :model="loginForm" ref="Form">
         <el-form-item>
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
@@ -18,7 +18,7 @@
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button type="primary" style="width:100%">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -37,7 +37,6 @@ export default {
   }
 }
 </script>
-
 <style scoped lang='less'>
 .container {
   width: 100%;
@@ -54,6 +53,9 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  text-align: center;
+  img {
+    display: block;
+    margin: 0 auto 30px;
+  }
 }
 </style>
