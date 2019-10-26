8add4b2ca6bf2781a6c17ff61ffc1aac3ec6b3fb
wszll123 <1551312275@qq.com>
2019-10-26 17:21:02 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
+import axios from '@/api'
+Vue.prototype.$http = axios
+// import axios from 'axios'
+// axios.default.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// export default axios
 // 导入文件的时候，默认导入的是目录下的索引文件（index.js index.vue index.json）。
 // @是webpack指定的路径别名  @===‘/src’   绝对路径
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,11 @@
   <div class="container">
     <el-card>
       <img src="../../assets/logo_index.png" width="200px" alt />
-      <el-form :model="loginForm" ref="Form">
-        <el-form-item>
-          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+      <el-form ref="loginForm" status-icon :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" status-icon placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="loginForm.code"
             style="width:235px;margin-right:10px"
@@ -18,7 +18,7 @@
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">立即登录</el-button>
+          <el-button @click="login" type="primary" style="width:100%">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,11 +28,47 @@
 <script>
 export default {
   data () {
+    const checkMobile = (rule, value, callBack) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callBack()
+      } else {
+        callBack(new Error('请输入正确的手机号'))
+      }
+    }
     return {
       loginForm: {
         mobile: '',
         code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码6个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          // 发请求 校验手机号和验证码  后台
+          this.$http
+            .post('authorizations', this.loginForm)
+            .then(res => {
+              // 成功
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 失败 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
@@ -48,7 +84,7 @@ export default {
 }
 .el-card {
   width: 400px;
-  height: 370px;
+  height: 340px;
   position: absolute;
   left: 50%;
   top: 50%;
