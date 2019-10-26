a15d6b31aeea8c8c90f4c7c9f98dcb29de834356
wszll123 <1551312275@qq.com>
2019-10-26 20:48:36 +0800

    主页的初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 配置对象
@@ -8,6 +11,19 @@ const router = new VueRouter({
   routes: [{
     path: '/login',
     component: Login
-  }]
+  },
+  {
+    path: '/',
+    component: Home,
+    children: [{
+      path: '/',
+      component: welcome
+    }]
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
+  ]
 })
 export default router
